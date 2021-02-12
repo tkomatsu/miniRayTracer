1b00118ac270db318f1864d4bc7f148a5810b316
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-12 22:55:33 +0900

    fix: multiply object color to specular

--- srcs/raytrace/specular.c
+++ srcs/raytrace/specular.c
@@ -6,7 +6,7 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 21:03:36 by tkomatsu          #+#    #+#             */
-/*   Updated: 2021/02/12 21:23:20 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/12 22:54:22 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,23 @@
 #define SHININESS 20.0
 #define SPECULAR 0.2
 
-double	specular_reflect(t_vec3 *ref_l, t_vec3 *rev_r, double lc)
+double	specular_reflect(t_vec3 *ref_l, t_vec3 *rev_r, double lc, double ic)
 {
 	double	res;
 	double	vr_dot;
 
 	vr_dot = fmax(fmin(vdot(*ref_l, *rev_r), 1.0), 0.0);
-	res = SPECULAR * lc * pow(vr_dot, SHININESS);
+	res = SPECULAR * lc * pow(vr_dot, SHININESS) * ic;
 	return (res);
 }
 
 int		multiply_specular(t_light *light, t_ip *intp, t_ray *ray, t_color *col)
 {
-	double	ratio;
 	double	nl_dot;
 	t_vec3	light_dir;
 	t_vec3	rev_r;
 	t_vec3	ref_l;
 
-	ratio = light->ratio /
-		(light->color.r + light->color.g + light->color.b);
 	light_dir = vsubtract(light->point, intp->point);
 	vunit(&light_dir);
 	nl_dot = fmax(fmin(vdot(light_dir, intp->normal), 1.0), 0.0);
@@ -44,9 +41,12 @@ int		multiply_specular(t_light *light, t_ip *intp, t_ray *ray, t_color *col)
 		vunit(&ref_l);
 		rev_r = vnproduct(ray->dir, -1);
 		vunit(&rev_r);
-		col->r += specular_reflect(&ref_l, &rev_r, light->color.r * ratio);
-		col->g += specular_reflect(&ref_l, &rev_r, light->color.g * ratio);
-		col->b += specular_reflect(&ref_l, &rev_r, light->color.b * ratio);
+		col->r += specular_reflect(&ref_l, &rev_r,
+				light->color.r * light->ratio, intp->color.r);
+		col->g += specular_reflect(&ref_l, &rev_r,
+				light->color.g * light->ratio, intp->color.g);
+		col->b += specular_reflect(&ref_l, &rev_r,
+				light->color.b * light->ratio, intp->color.b);
 	}
 	col->r = fmin(col->r, 1.0);
 	col->g = fmin(col->g, 1.0);
